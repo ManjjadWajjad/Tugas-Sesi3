5ea991f80fcd23a2566d7d7797e25fbffc614956
ManjjadWajjad <muhammadfaisalmuzhaffar@gmail.com>
2024-03-03 13:39:32 +0700

    first commit

---
+++ TugasSesi3.py
@@ -0,0 +1,10 @@
+uang_udin = 100000
+harga_rokok = 20000
+harga_minyak = 25000
+harga_gula = 11000
+diskon = 10000
+
+total_harga = harga_rokok + harga_minyak + harga_gula - diskon
+Kembalian_ucup = uang_udin - total_harga
+
+print("Jumlah kembalian ucup adalah: ", Kembalian_ucup)
